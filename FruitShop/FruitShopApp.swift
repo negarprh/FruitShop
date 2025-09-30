b69752eccadf71c813dda57a07e64b8848026640
Negar <negarpr@hotmail.com>
2025-09-30 16:04:24 -0400

    Initial Commit

---
+++ FruitShop/FruitShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitShopApp.swift
+//  FruitShop
+//
+//  Created by Negar Pirasteh on 2025-09-30.
+//
+
+import SwiftUI
+
+@main
+struct FruitShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
